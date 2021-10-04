2f4a5eec87dc0dcc12cfd9994ceae4828b0dd812
Olga Bordiug <obordiug.gmail.com>
2021-10-04 12:22:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+const BASE_URL
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+const DETAILS_URL
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+const logo = document.querySelector('.logo');
+const list = document.createElement('ul');
+
+const getPhones = () => {
+  return fetch(BASE_URL)
+    .then(response => {
+      if (!response.ok) {
+        setTimeout(() => new Error(`${response.status}`), 5000);
+      }
+
+      return response.json();
+    });
+};
+
+const getPhonesDetails = () => {
+  const phonesId = [];
+
+  getPhones()
+    .then(phones => {
+      phones.forEach(phone => phonesId.push(phone.id));
+
+      const phonesDetails = phonesId.map(itemId =>
+        fetch(`${DETAILS_URL}${itemId}.json`)
+          .then(response => response.json()));
+
+      Promise.all(phonesDetails)
+        .then(details => details.forEach(phone => addPhoneToList(phone.name)));
+    })
+    .catch(error => new Error(error));
+};
+
+getPhonesDetails();
+
+function addPhoneToList(phonesName) {
+  list.insertAdjacentHTML('beforeend', `<li>${phonesName}</li>`);
+};
+
+logo.after(list);
